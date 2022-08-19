892df03a7b81115b448c1a2436c2ead12c64fa1a
MichaelRep <michaelreptin@gmail.com>
2022-08-19 12:20:26 +0200

    Add Spring Annotation for repository/EmployeeRepository.java

--- src/main/java/com/mreptin/project/hrassociation/repository/EmployeeRepository.java
+++ src/main/java/com/mreptin/project/hrassociation/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.mreptin.project.hrassociation.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mreptin.project.hrassociation.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
